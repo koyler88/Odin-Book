255271b33b8388eebd175891620f9b1b0a368888
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-09-02 15:54:53 -0600

    feat(backend): add express middleware to app, grab port from .env file

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello there!")
 })
 
-app.listen(3000, () => console.log("App running on port 3000"))
+app.listen(PORT, () => console.log(`backend running on port ${PORT}`))
