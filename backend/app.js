aca3671e8b33a7fcbf5f4100158ae1db64d17855
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-09-08 16:52:14 -0600

    feat(backend): hook passport into app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const configurePassport = require("./config/passport");
+const passport = require("passport");
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +12,10 @@ const authRouter = require("./routes/authRouter");
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Passport setup
+configurePassport(passport);
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
   res.send("Hello there!");
